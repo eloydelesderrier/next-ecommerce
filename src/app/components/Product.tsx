44ef8a995c40aaff4d485fc003c6a63675fda214
eloydelesderrier <eloy.delesderrier@hotmail.com>
2025-01-14 17:12:55 -0300

    Estilizando componentes produto com Next Image

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,4 +1,6 @@
 import { ProductType } from "@/types/Product.Type";
+import Image from "next/image";
+import ProductImage from "./ProductImage";
 
 type ProductProps = {
     product:ProductType
@@ -8,8 +10,19 @@ export default function Product({product}:ProductProps) {
 
     return(
         <div className="flex flex-col shadow-lg h-96 bg-gray-950 p-5 text-gray-400">
-            <div className="relative max-h-72 flex-1">IMG</div>
-            <div className="flex justify-between font-bols my-3">{product.title}</div>
+            <div className="relative max-h-72 flex-1">
+                <ProductImage product={product} fill
+                />
+            </div>
+            <div className="flex justify-between font-bols my-3">
+                <p className="w-40 truncate">
+                    {product.title}
+                </p> 
+                
+                <p className="text-md text-teal-400">
+                    ${product.price}
+                </p> 
+            </div>
             <button className="roundend-md bg-teal-600 text-white p-3.5 py-2.5 text-sm text-center">Adicionar ao Carrinho</button>
         </div>
     )
